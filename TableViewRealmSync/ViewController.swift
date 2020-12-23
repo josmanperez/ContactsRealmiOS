b4e3192736073bbcefa57acb329365ce8ea0cccf
Josman Pérez <josman.perez@mongodb.com>
2020-12-23 17:20:10 +0100

    * Show static tableView

--- TableViewRealmSync/ViewController.swift
+++ TableViewRealmSync/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
+    }
+    
+    /// Initial configuration for the tableView
+    func configureTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "Hola"
+        return cell
+        
+    }
+    
+    
+    
 }
 
