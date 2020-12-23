49ce34e5c3ff8fee56d4e5de946d111780566253
Josman Pérez <josman.perez@mongodb.com>
2020-12-23 17:12:44 +0100

    Initial Commit

---
+++ TableViewRealmSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewRealmSync
+//
+//  Created by Josman Pedro Pérez Expósito on 23/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
